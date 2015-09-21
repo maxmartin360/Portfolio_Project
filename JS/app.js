b6f0b24eaea222e367115ba390123095307fd61d
Max Martin <maxmartin360@gmail.com>
2015-09-20 22:25:44 -0400

    initial commit

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+var ctx = document.getElementById("myChart").getContext("2d");
+var myRadarChart = new Chart(ctx).Radar(data, options);
+
+var data = {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+    ]
+};
